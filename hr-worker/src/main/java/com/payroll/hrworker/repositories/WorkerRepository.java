0fefc2f4178d60f8fbd4a3aff9a06622867c8660
Gabriel Araújo <gabrielaraujogpa@gmail.com>
2021-03-07 13:18:31 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/payroll/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,7 @@
+package com.payroll.hrworker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.payroll.hrworker.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long> { }
