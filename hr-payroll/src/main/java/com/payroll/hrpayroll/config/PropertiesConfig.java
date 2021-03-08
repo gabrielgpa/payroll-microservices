c146a678e393d6c47165234bf79d4e3c7681f9e9
Gabriel Araújo <gabrielaraujogpa@gmail.com>
2021-03-07 21:10:49 -0300

    Feign client

--- hr-payroll/src/main/java/com/payroll/hrpayroll/config/PropertiesConfig.java
+++ hr-payroll/src/main/java/com/payroll/hrpayroll/config/PropertiesConfig.java
@@ -1,20 +1,10 @@
 package com.payroll.hrpayroll.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PropertiesConfig {
 	
-	@Value("${hr-worker.host}")
-	String workerHost;
-
-	public String getWorkerHost() {
-		return workerHost;
-	}
-
-	public void setWorkerHost(String workerHost) {
-		this.workerHost = workerHost;
-	}
+	
 	
 }
--- hr-payroll/src/main/java/com/payroll/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/payroll/hrpayroll/services/PaymentService.java
@@ -1,34 +1,20 @@
 package com.payroll.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import com.payroll.hrpayroll.config.PropertiesConfig;
 import com.payroll.hrpayroll.entities.Payment;
 import com.payroll.hrpayroll.entities.Worker;
+import com.payroll.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
-	
+		
 	@Autowired
-	private PropertiesConfig properties;
-	
-	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(Long workerId, int days) {
-		Map<String, String> uriVariable = new HashMap<>();
-		uriVariable.put("id", Long.toString(workerId));
-		
-		Worker worker = restTemplate.getForObject(
-				properties.getWorkerHost().concat("workers/{id}"),
-				Worker.class, 
-				uriVariable
-		);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
